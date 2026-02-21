caaba32d30a8dd3c9011a001106627fad16faa2f
Anurag Kumar Singh <anuragkumarsingh651@gmail.com>
2026-02-22 03:15:40 +0530

    update

---
+++ Array/p_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int arr[5] = {2,3,4,5,6};
+    arr[4] = 8;
+
+    float a[3] = {1.2, 3.4, 5.6}; 
+
+    printf("%d\n", arr[4]); 
+    printf("%f\n", a[2]);   
+
+    return 0;
+}
